c41c882249ab72050f7988f22d93782bfbfaa858
Wamgleb <kalinchukgleb1@gmail.com>
2020-05-22 20:32:11 +0300

    Ver-1

---
+++ README.md
@@ -0,0 +1,8 @@
+#DLL.Scanner
+The program allows you to get all the functions of the Windows API with malware.
+
+You need: pip install pefile.
+
+How use?
+
+> python3 dll.py Viruse.exe
---
+++ dll.py
@@ -0,0 +1,21 @@
+import pefile
+import sys
+
+print('''
+ mmmm   m      m              mmmm                                           
+ #   "m #      #             #"   "  mmm    mmm   m mm   m mm    mmm    m mm 
+ #    # #      #             "#mmm  #"  "  "   #  #"  #  #"  #  #"  #   #"  "
+ #    # #      #                 "# #      m"""#  #   #  #   #  #""""   #    
+ #mmm"  #mmmmm #mmmmm   #    "mmm#" "#mm"  "mm"#  #   #  #   #  "#mm"   #    by Wam11\n''')
+
+mal_file = sys.argv[1]
+pe = pefile.PE(mal_file)
+if hasattr(pe, 'DIRECTORY_ENTRY_IMPORT'):
+    for entry in pe.DIRECTORY_ENTRY_IMPORT:
+        print("{}".format(entry.dll))
+        for imp in entry.imports:
+            if imp.name != None:
+                print("\t{}".format(imp.name))
+            else:
+                print("\tord {}".format(str(imp.ordinal)))
+        print("\n")
